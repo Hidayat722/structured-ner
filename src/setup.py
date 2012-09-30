71f3cebed8fa1d97d0b4b4cd2a7ddd6626b72b1f
Jo Daiber <jodaiber@gmx.de>
2012-09-30 11:52:58 +0200

    Moving things around, setting up python project.

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Structured NER',
+      version='0.1',
+      description='NER using Structured Prediction',
+      #author='Joachim Daiber',
+      #author_email='gward@python.net',
+      url='https://github.com/jodaiber/structured-ner',
+      packages=['nltk'],
+     )
