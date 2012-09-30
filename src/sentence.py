6d9dd8df578b8e33754b9a4cd7d26950263f9685
Jo Daiber <jodaiber@gmx.de>
2012-10-01 01:18:35 +0200

    Added reading from CoNLL corpus, basic training.

--- src/sentence.py
+++ src/sentence.py
@@ -1,5 +1,23 @@
+def sentence_from_conll(chunked_sent):
+    x = []
+    y = []
+
+    for element in chunked_sent:
+        if type(element) == tuple:
+            x.append(element)
+            y.append("O")
+        else:
+            for ne_token in element.leaves():
+                x.append(ne_token)
+                y.append(element.node)
+
+    return Sentence(x, y)
+
 class Sentence:
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def __str__(self):
+        return " ".join(map(lambda (a,b): a[0]+"/"+a[1]+"/"+b, zip(self.x, self.y)))
--- src/train_supervised.py
+++ src/train_supervised.py
@@ -1,4 +1,14 @@
-from nltk.corpus import conll2002
+from nltk.corpus import conll2002, conll2000
+from feature_generator import SimpleFeatureGenerator
+from sentence import Sentence, sentence_from_conll
 from structured_perceptron import StructuredPerceptron
 
-perceptron = StructuredPerceptron()
+#We are using simple features:
+feature_generator = SimpleFeatureGenerator()
+
+#Create the Perceptron
+perceptron = StructuredPerceptron(conll2002._chunk_types, feature_generator)
+
+#Train it!
+perceptron.train([sentence_from_conll(sent) for sent in conll2002.chunked_sents('ned.train')])
+
