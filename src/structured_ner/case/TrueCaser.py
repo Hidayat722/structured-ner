cd933470470609bd743913cbbc5d696c18b13140
Jo Daiber <jodaiber@gmx.de>
2012-11-08 20:26:50 +0100

    .

--- src/structured_ner/case/TrueCaser.py
+++
@@ -1,8 +0,0 @@
-class TrueCaser:
-
-    def case_pairs(self, token_tags):
-        tokens, tags = zip(*token_tags)
-        return self.case(list(tokens), list(tags))
-
-    def case(self, tokens, pos_tags):
-        pass
